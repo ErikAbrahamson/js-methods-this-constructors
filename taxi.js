84555b89be5b65a8b02c51c48c38de9a26e4d08c
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-08-08 14:27:30 -0600

    finish exercise

--- taxi.js
+++ taxi.js
@@ -15,15 +15,18 @@ Taxi.prototype.passengerCount = function() {
 };
 
 Taxi.prototype.full = function() {
-  if (this.passengers.length - 1 > this.maxNumberOfPassengers) {
+  if (this.passengers.length < this.maxNumberOfPassengers) {
     return false;
   } else {
     return true;
   }
 };
 
-
-
-
+Taxi.prototype.dropOffPassengers = function() {
+  for (var i = this.passengers.length; i > 0; i--) {
+    this.passengers.pop(i);
+  }
+  return this.passengers;
+};
 
 module.exports = Taxi;
